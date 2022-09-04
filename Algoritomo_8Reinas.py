95673b77a23caf821b14aa60c15915bde6f9f69e
saulsavh <saulsavh@gmail.com>
2022-09-04 17:01:15 -0600

    Intento de solucion usando funcion backtraking -Aun sin resolver

---
+++ Algoritomo_8Reinas.py
@@ -0,0 +1,56 @@
+""" 
+Elaborar un programa para ubicar 8 reainas en un tablero
+8 x 8 sin que estas se ataquen entre si  
+"""
+#%%
+""" 
+Generamos el tablero  donde las reinas estaran ubicadas
+Las reinas se encontraran en un espacio limitado por el indice i y su valor k
+La posicion del la reina será Pos_reinas[i] = k
+"""
+
+
+Pos_reinas = [-1, -1, -1, -1]
+
+"""
+La posicion del la reina 1 será Pos_reinas[i] = k y por comparacion la reina 2 sera Pos_reinas[x] = y
+Para limitar las coliciones las condiciones seran i no puede ser igual a x.              (i != x)
+Del mismo modo k reina 1 no puede ser igual a (y) reina 2,                               (k != y)
+Para la diagonal izq el valor (k-i) de reina 1 no puede ser igual al (y-x) de reina 2,   ((k-i) != (y-x))
+Para la diagonal der el valor |(i-x)| no debe ser igual al valor |(k-y)|,  |(i-x)| != |(k-y)|
+"""
+def comp_pos(k, Pos_reinas, num_reinas):
+    i = 0
+    while i<num_reinas:
+        if (i == k or Pos_reinas[i] == Pos_reinas[k] or abs(i - k) == abs(Pos_reinas[i]- Pos_reinas[k])):
+            return False
+
+def backtraking(nivel, num_reinas, Pos_reinas ):
+
+    if nivel == num_reinas:
+        print(f"Solucion es ")
+        for i in range(num_reinas):
+            print(f"({i}, {Pos_reinas[i]}), ")
+        return 0
+
+    else:
+        for k in range(num_reinas):
+            if ( comp_pos(k, Pos_reinas, num_reinas)):
+                backtraking(nivel+1, num_reinas, Pos_reinas)
+
+
+
+# %%
+
+nivel = 0
+num_reinas = 8
+
+for i in range (8):
+    Pos_reinas.append(-1)
+
+
+# %%
+print("inicio")
+backtraking(nivel, num_reinas, Pos_reinas)
+
+# %%
