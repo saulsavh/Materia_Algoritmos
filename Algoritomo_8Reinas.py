4eaf6b51b8655e1432cc2340d01d401e2b6649ad
Saul Villalvazo <saulsavh@gmail.com>
2022-09-09 13:00:47 -0600

    Actualizacion condicional -no resuelto

--- Algoritomo_8Reinas.py
+++ Algoritomo_8Reinas.py
@@ -21,9 +21,10 @@ Para la diagonal der el valor |(i-x)| no debe ser igual al valor |(k-y)|,  |(i-x
 """
 def comp_pos(k, Pos_reinas, num_reinas):
     cont = 0
-    if (cont == k): cont = cont+1
+    
     while cont<num_reinas:
-        if (Pos_reinas[cont] == Pos_reinas[k] or abs(cont - k) == abs(Pos_reinas[cont]- Pos_reinas[k]) or (k-Pos_reinas[k]) == (cont - Pos_reinas[cont])):
+        if (cont == k): cont = cont+1
+        if (Pos_reinas[cont] == Pos_reinas[k] or (cont - k) == (Pos_reinas[cont]- Pos_reinas[k]) or abs(k-Pos_reinas[cont]) == abs(cont - Pos_reinas[k])):
         #if (cont == k or Pos_reinas[cont] == Pos_reinas[k]):
             return False
         cont = cont+1
